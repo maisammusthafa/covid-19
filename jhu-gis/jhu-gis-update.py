28bd9b72b8c1b260e0b12100bfc29c527a59337c
Maisam Musthafa <maisam.musthafa@gmail.com>
2020-03-24 16:11:33 +0530

    Add jhu-gis script and initial data.

---
+++ jhu-gis/jhu-gis-update.py
@@ -0,0 +1,43 @@
+#!/bin/env python3
+
+import pandas
+from arcgis.gis import GIS
+from arcgis.features import FeatureLayer
+from datetime import datetime
+
+layer_global = FeatureLayer('https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2')
+query_global = layer_global.query(where='1=1', out_fields='Country_Region, Confirmed, Deaths, Recovered, Active')
+df_global = query_global.sdf.drop(columns=['OBJECTID', 'SHAPE']).sort_values(by=['Country_Region'])
+
+today = datetime.today().strftime('%Y-%m-%d')
+categories = ['Confirmed', 'Deaths', 'Recovered', 'Active']
+
+def write_to_excel(data_frame, file_name):
+    with pandas.ExcelWriter(file_name, engine='xlsxwriter') as writer:
+        for category in categories:
+            data_frame[category].to_excel(writer, sheet_name=category, index=False)
+
+        for sheet in writer.sheets.values():
+            sheet.set_column('A:A', 28)
+            sheet.set_column('B:Z', 10)
+            sheet.freeze_panes(1, 1)
+
+    writer.save()
+
+df = {}
+df_new = {}
+
+for category in categories:
+    df[category] = pandas.read_excel('data/jhu_gis_total.xlsx', sheet_name=category).drop(columns=[today], errors='ignore')
+    df_new[category] = df_global[['Country_Region', category]].rename(columns={category: today})
+
+    df[category] = pandas.merge(df[category], df_new[category], on='Country_Region', validate='one_to_one')
+    # df[category] = df[category].reset_index()
+    # df[category] = df[category].set_index('Country_Region').T.rename_axis('Date').reset_index()
+
+write_to_excel(df, 'data/jhu_gis_total.xlsx')
+
+for category in categories:
+    df[category] = df[category].set_index('Country_Region').diff(axis=1).reset_index()
+
+write_to_excel(df, 'data/jhu_gis_diff.xlsx')
