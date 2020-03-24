af743b8cf3ec0d2b9eec82c63b332c70dd22b9d5
Maisam Musthafa <maisam.musthafa@gmail.com>
2020-03-24 18:52:51 +0530

    Update jhu-gis script to generate daily totals.

--- jhu-gis/jhu-gis-update.py
+++ jhu-gis/jhu-gis-update.py
@@ -12,10 +12,13 @@ df_global = query_global.sdf.drop(columns=['OBJECTID', 'SHAPE']).sort_values(by=
 today = datetime.today().strftime('%Y-%m-%d')
 categories = ['Confirmed', 'Deaths', 'Recovered', 'Active']
 
-def write_to_excel(data_frame, file_name):
+def write_to_excel(file_name, data_frame, single_sheet=False):
     with pandas.ExcelWriter(file_name, engine='xlsxwriter') as writer:
-        for category in categories:
-            data_frame[category].to_excel(writer, sheet_name=category, index=False)
+        if single_sheet:
+            data_frame.to_excel(writer, sheet_name=today, index=False)
+        else:
+            for category in categories:
+                data_frame[category].to_excel(writer, sheet_name=category, index=False)
 
         for sheet in writer.sheets.values():
             sheet.set_column('A:A', 28)
@@ -35,9 +38,11 @@ for category in categories:
     # df[category] = df[category].reset_index()
     # df[category] = df[category].set_index('Country_Region').T.rename_axis('Date').reset_index()
 
-write_to_excel(df, 'data/jhu_gis_total.xlsx')
+write_to_excel('data/jhu_gis_total.xlsx', df)
 
 for category in categories:
     df[category] = df[category].set_index('Country_Region').diff(axis=1).reset_index()
 
-write_to_excel(df, 'data/jhu_gis_diff.xlsx')
+write_to_excel('data/jhu_gis_diff.xlsx', df)
+
+write_to_excel('data/daily_totals/{0}_jhu_gis_total.xlsx'.format(today), df_global, single_sheet=True)
