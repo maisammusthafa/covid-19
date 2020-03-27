d80589c71c211a61a4b1923ca336af85e38f142f
Maisam Musthafa <maisam.musthafa@gmail.com>
2020-03-27 19:10:45 +0530

    Update ecdc-csv script to source and process data directly from ECDC.

--- ecdc-csv/ecdc-csv-update.py
+++ ecdc-csv/ecdc-csv-update.py
@@ -3,33 +3,36 @@
 import pandas
 import urllib.request
 
-source_data_url = 'https://covid.ourworldindata.org/data/ecdc/full_data.csv'
+source_data_url = 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv'
 urllib.request.urlretrieve(source_data_url, 'source_data.csv')
 
-df_source = pandas.read_csv('source_data.csv')
-
-categories = {
-        'new_cases': 'Confirmed',
-        'new_deaths': 'Deaths',
-        'total_cases': 'Confirmed',
-        'total_deaths': 'Deaths'
-        }
-
+df_source = pandas.read_csv('source_data.csv', encoding='latin1')
+categories = ['cases', 'deaths']
 dfs = {}
 
 for category in categories:
-    dfs[category] = df_source[['date', 'location', category]]
-    dfs[category] = dfs[category].pivot(index='date', columns='location', values=category)
-    dfs[category] = pandas.DataFrame(dfs[category].to_records()).rename(columns={'date': 'Date'})
+    dfs[category] = df_source[['dateRep', 'countriesAndTerritories', category]]
+    dfs[category] = dfs[category].pivot(index='dateRep', columns='countriesAndTerritories', values=category)
+
+    dfs[category] = pandas.DataFrame(dfs[category].to_records()).rename(columns={'dateRep': 'Date'})
+    dfs[category]['Date'] = pandas.to_datetime(dfs[category]['Date'], format='%d/%m/%Y').dt.strftime('%Y-%m-%d')
+    dfs[category] = dfs[category].sort_values(by=['Date'])
+
+    dfs[category].columns = dfs[category].columns.str.replace('_', ' ')
+
+dfs['new_cases'] = dfs['cases']
+dfs['new_deaths'] = dfs['deaths']
+dfs['total_cases'] = dfs['cases'].set_index('Date').cumsum().reset_index()
+dfs['total_deaths'] = dfs['deaths'].set_index('Date').cumsum().reset_index()
 
 def write_to_excel(file_name, data_categories):
     with pandas.ExcelWriter(file_name, engine='xlsxwriter') as writer:
-        for category in data_categories:
-            dfs[category].to_excel(writer, sheet_name=categories[category], index=False)
+        for category, name in data_categories.items():
+            dfs[category].to_excel(writer, sheet_name=name, index=False)
 
         for sheet in writer.sheets.values():
             sheet.set_column('A:A', 12)
             sheet.freeze_panes(1, 1)
 
-write_to_excel('data/ecdc_csv_total.xlsx', ['total_cases', 'total_deaths'])
-write_to_excel('data/ecdc_csv_diff.xlsx', ['new_cases', 'new_deaths'])
+write_to_excel('data/ecdc_csv_total.xlsx', {'total_cases': 'Confirmed', 'total_deaths': 'Deaths'})
+write_to_excel('data/ecdc_csv_diff.xlsx', {'new_cases': 'Confirmed', 'new_deaths': 'Deaths'})
